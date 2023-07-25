9fbbc93950b7334cc81e193bbb63f534d8f0336a
Scott Sun <minghuisunscott@gmail.com>
2023-07-25 00:05:03 -0400

    Update data_utils.py

--- data_utils.py
+++ data_utils.py
@@ -119,3 +119,183 @@ class DataLoader(Sequence):
         for var_name in self._categorical_vars_info.keys():
             X_columns.append(var_name)
         self._rawX = self._rawX[X_columns]
+
+    @property
+    def continuous_vars_info(self) -> dict:
+        """Get continuous_vars_info."""
+        return self._continuous_var_info
+
+    @property
+    def categorical_vars_info(self) -> dict:
+        """Get categorical_vars_info."""
+        return self._categorical_vars_info
+
+    @property
+    def continuous_data(self) -> np.ndarray:
+        """Get data from the continuous variables."""
+        return self._rawX[self._continuous_vars_info.keys()].values
+
+    @property
+    def continuous_indctr_data(self) -> np.ndarray:
+        """Get a indicator matrix w.r.t. the continuous data. 0 := missing, 1 := observed."""
+        return 1 - np.isnan(self.continuous_data)
+
+    @property
+    def categorical_data(self) -> np.ndarray:
+        """Get data from categorical variables in cat mode format."""
+        return self._rawX[self._categorical_vars_info.keys()].values
+
+    @property
+    def n_sample(self) -> int:
+        """Get the number of samples."""
+        return self._n_samples
+
+    @abstractclassmethod
+    def __getitem__(self, index) -> tuple:
+        """Get a batch data."""
+        pass
+
+    @abstractclassmethod
+    def __len__(self) -> int:
+        """Get the total number of batches per epoch."""
+        pass
+
+    @abstractclassmethod
+    def on_epoch_end(self) -> None:
+        """Shuffle data after each epoch."""
+        pass
+
+
+class DataSeqLoader(DataLoader):
+    """DataSeqLoader, a subclass of DataLoader, which reshape the data into DataSequence objects."""
+
+    def __init__(
+        self, X: pd.DataFrame, y: pd.Series, ids: pd.Series, batch_size: int
+    ) -> None:
+        """Init."""
+        super().__init__(X, y, ids, batch_size)
+        self._populate_data_seq()
+
+    def _populate_data_seq(self) -> None:
+        """Reshape data as DataSequence."""
+        data = self._rawX.copy()
+        data[self._rawy.name] = self._rawy
+        grouped_data = data.groupby(data.index.name)
+        timesteps = grouped_data[self._rawy.name].count()
+        # initialize
+        data_seqs: list[DataSequence] = [
+            DataSequence(
+                timesteps=t,
+                batch_size=self._batch_size,
+                columns=data.columns,
+                y_var_name=self._rawy.name,
+            )
+            for t in range(1, 15)
+        ]
+        # start populating
+        for pid in tqdm(timesteps.indices):
+            t = timesteps.get(pid)
+            data_seqs[t - 1].add(grouped_data, pid)
+        self.data_seqs = data_seqs
+
+    def __getitem__(self, index) -> tuple:
+        """Get a batch by a given index."""
+        for seq in self.data_seqs:
+            if index < len(seq):
+                return seq[index]
+            else:
+                index -= len(seq)
+        raise ValueError("index out of upper bound.")
+
+    def __len__(self) -> int:
+        """Get the size of the object."""
+        return sum([len(data_seq) for data_seq in self.data_seqs])
+
+    def on_epoch_end(self) -> None:
+        """Shuffle sample randomly by the end of an epoch."""
+        for data_seq in self.data_seqs:
+            data_seq.shuffle()
+
+
+class DataTableLoader(DataLoader):
+    """DataTableLoader, a subclass of DataLoader, which maintain the tabular format of the raw data."""
+
+    def __init__(
+        self,
+        X: pd.DataFrame,
+        y: pd.Series,
+        ids: pd.Series,
+        batch_size: int,
+        balance: bool = True,
+    ) -> None:
+        """Init."""
+        super().__init__(X, y, ids, batch_size)
+        self._balance = balance
+        self._datalen = len(self._rawy)
+        self._num_classes = len(np.unique(self._rawy))
+        self._X = [self._rawX.values[:, i] for i in range(self._rawX.shape[1])]
+        self._y = self._rawy.values
+        self._indices = []
+        for c in range(self._num_classes):
+            self._indices.append(np.arange(self._datalen)[self._rawy.values == c])
+
+    def __getitem__(self, index) -> tuple:
+        """Get the batch by the given index."""
+        # first obtain the row-level indices for the batch specified by the time index
+        if self._balance:
+            n = int(self._batch_size / 2)
+            batch_indices_list = []
+            for c in range(self._num_classes):
+                _batch_indices = circular_slicing(self._indices[c], index * n, n)
+                batch_indices_list.append(_batch_indices)
+            batch_indices = np.concatenate(batch_indices_list)
+        else:
+            start = np.min([index * self._batch_size, self._rawy.size])
+            end = np.min([(index + 1) * self._batch_size, self._rawy.size])
+            batch_indices = np.concatenate(self._indices)[start:end]
+        # obtain the batch data
+        X_batch = [elem[batch_indices] for elem in self._X]
+        y_batch = self._y[batch_indices]
+        return X_batch, y_batch
+
+    def __len__(self) -> int:
+        """Get the number of the batches."""
+        if self._balance:
+            return self._datalen // int(self._batch_size / 2)
+        else:
+            return self._datalen // self._batch_size
+
+    def on_epoch_end(self) -> None:
+        """Shuffle samples randomly by the end of an epoch."""
+        for c in range(self._num_classes):
+            np.random.shuffle(self._indices[c])
+
+
+def train_test_split(
+    df: pd.DataFrame, frac: float
+) -> tuple[pd.DataFrame, pd.DataFrame]:
+    """Split data into train set and validation set."""
+    if frac < 0.0 or frac > 1.0:
+        raise ValueError("Incorrect value for frac, which should be in [0, 1]")
+    pid = np.unique(df["PAT_ENC_CSN_ID"].values)
+    N = len(pid)
+    pid_valid = np.random.choice(pid, int(frac * N))
+    valid = df.loc[df["PAT_ENC_CSN_ID"].isin(pid_valid)]
+    train = df.loc[~df["PAT_ENC_CSN_ID"].isin(pid_valid)]
+    return (train, valid)
+
+
+def circular_slicing(arr: np.ndarray, start: int, size: int):
+    """Slice the given array in a circular way.
+
+    The cursor will be reset to the beginning for large end indices so that the extended portion is included.
+    """
+    arr_len = len(arr)
+    start = start % arr_len
+    if size > arr_len:
+        raise ValueError("maybe consider smaller batch size.")
+    if start + size > arr_len:
+        return np.append(arr[start:], arr[: (start + size - arr_len)])
+    else:
+        end = start + size
+        return arr[start:end]
